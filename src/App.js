8d35c887d37930d32d3ffbf7571286da7467dad6
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-09-23 16:29:50 -0500

    Added header component and links to pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage}></Route>
-      <Route path="/shop" component={ShopPage}></Route>
-    </Switch>
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
